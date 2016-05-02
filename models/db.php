999deab00406466319191d1a45dab190d98fa0ae
Pavel Chistyakov <paha710@yandex.ru>
2016-05-02 17:29:35 +0300

    Delete db.php

--- models/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=calendar',
-    'username' => 'root',
-    'password' => '0000',
-    'charset' => 'utf8',
-];
