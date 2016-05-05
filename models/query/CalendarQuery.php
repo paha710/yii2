091c464fc84ebf8afc633c446176ac985d1fffe5
pavel <paha710@yandex.ru>
2016-05-05 17:58:54 +0300

    Созданы модели AccessQuery,CalendarQuery,UserQuery

---
+++ models/query/CalendarQuery.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\models\query;
+
+/**
+ * This is the ActiveQuery class for [[\app\models\Calendar]].
+ *
+ * @see \app\models\Calendar
+ */
+class CalendarQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        $this->andWhere('[[status]]=1');
+        return $this;
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return \app\models\Calendar[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\Calendar|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
