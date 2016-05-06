c3cf95d6e1ffd8455360d76b7eb1628b0729ec9b
paha710 <paha710@yandex.ru>
2016-05-06 08:51:10 +0300

    Create model Calendar

---
+++ models/Calendar.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "calendar".
+ *
+ * @property integer $id
+ * @property string $text
+ * @property integer $creator
+ * @property string $date_event
+ */
+class Calendar extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'calendar';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['text', 'creator', 'date_event'], 'required'],
+            [['text'], 'string'],
+            [['creator'], 'integer'],
+            [['date_event'], 'safe']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'text' => Yii::t('app', 'Text'),
+            'creator' => Yii::t('app', 'Creator'),
+            'date_event' => Yii::t('app', 'Date Event'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\query\CalendarQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \app\models\query\CalendarQuery(get_called_class());
+    }
+}
