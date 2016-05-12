f601f6649d209467befbb12aee71abc752afc5f2
paha710 <paha710@yandex.ru>
2016-05-12 13:16:09 +0300

    Update AccessSearch

--- models/search/AccessSearch.php
+++ models/search/AccessSearch.php
@@ -6,7 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Access;
-
+use app\models\User;
 /**
  * AccessSearch represents the model behind the search form about `app\models\Access`.
  */
