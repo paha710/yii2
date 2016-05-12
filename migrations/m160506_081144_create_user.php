a751b57db18ba009b85836deeae4003ea25c080e
paha710 <paha710@yandex.ru>
2016-05-12 13:12:34 +0300

    Update migrations

--- migrations/m160506_081144_create_user.php
+++ migrations/m160506_081144_create_user.php
@@ -1,25 +1,26 @@
 <?php
+
 use yii\db\Migration;
 
-class m160505_062251_create_clndr_user extends Migration
+class m160506_081144_create_user extends Migration
 {
     public function safeUp()
     {
-    $this->createTable('clndr_user', [
+        $this->createTable('user', [
             'id'=>$this->primaryKey(),
             'username'=>$this->string(128)->notNull()->unique(),
             'name'=>$this->string(45)->notNull(),
             'surname'=>$this->string(45)->notNull(),
             'password'=>$this->string(255)->notNull(),
             'salt'=>$this->string(255)->notNull(),
-            'access_token' => $this->string(255)->notNull()->defaultValue('NULL')->unique(),
-            'create_date' => $this->dateTime()->notNull()
-    ]);
+            'access_token' => $this->string(255)->notNull()->unique(),
+            'create_date' => $this->timestamp()->notNull()
+        ]);
     }
 
     public function safeDown()
     {
-    $this->dropTable('clndr_user');
+        $this->dropTable('user');
     }
-    
+
 }
